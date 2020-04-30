68805999620f5995613d07dfb3a3fd2430870439
leye20 <leyeagbo@gmail.com>
2020-04-30 00:31:50 -0600

    forms display

--- index.php
+++ index.php
@@ -1,34 +1,32 @@
 <?php
-  // This file's code will execute right here in the file.
-  include './includes/datatypes.php';
+  $myTitle = 'My First To-Do List with PHP';
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo $myMessage; // Echo is used for outputting strings. ?></title>
+  <title><?php echo $myTitle?></title>
 </head>
 <body>
-  <h1><?php echo $myMessage; ?></h1>
-  <?php include './includes/navigation.php'; // We're executing this code right here... the nav will display. ?>
-  <pre>
-    <?php
-      // Var dump is great for seeing what's inside!
-      var_dump( $myOtherArray );
-    ?>
-  </pre>
-  <h2>Concatenated String</h2>
-  <p>
-    <?php echo $concattedString; ?>
-  </p>
-  <h2>Difference Between Single and Double Quoted Strings</h2>
-  <h3>Single Quoted</h3>
-  <p>
-    <?php echo $mySingleQuoteHelloString; ?>
-  </p>
-  <h3>Double Quoted</h3>
-  <p>
-    <?php echo $myDoubleQuoteHelloString; ?>
-  </p>
+  <h3><?php echo $myTitle?><h3>
+
+  <form action="" method="GET">
+    <label for="todo">Add a to-do</label><br>
+    <input type="text" name="todo" id="todo">
+    <input type="submit" value="Add to List">
+    <input type="submit" value="Reset">
+  </form>
+
+  <h3>Active To-Do<h3>
+
+  <form action="" method="GET">
+    <input type="checkbox" name="activetodo" id="activetodo">
+  </form>
+  
+  <h3><To-Dos>Completed</To-Dos><h3>
+
+  <form action="" method="GET">
+    <input type="list" name="completetodo" id="completetodo">
+  </form>
 </body>
 </html>
