5cf57c60b571e34b266f007c465fb7486f189a73
leye20 <leyeagbo@gmail.com>
2020-05-02 02:03:17 -0600

    API Test Assigment Done

--- index.php
+++ index.php
@@ -1,34 +1,37 @@
 <?php
-  // This file's code will execute right here in the file.
-  include './includes/datatypes.php';
+
+$randomTriviaJSONString = file_get_contents( 'https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=boolean' );
+
+$randomTriviaObject = json_decode( $randomTriviaJSONString );
+
+$randomTrivia = $randomTriviaObject->results[0]
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo $myMessage; // Echo is used for outputting strings. ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Assignment</title>
 </head>
 <body>
-  <h1><?php echo $myMessage; ?></h1>
-  <?php include './includes/navigation.php'; // We're executing this code right here... the nav will display. ?>
-  <pre>
-    <?php
-      // Var dump is great for seeing what's inside!
-      var_dump( $myOtherArray );
-    ?>
-  </pre>
-  <h2>Concatenated String</h2>
-  <p>
-    <?php echo $concattedString; ?>
-  </p>
-  <h2>Difference Between Single and Double Quoted Strings</h2>
-  <h3>Single Quoted</h3>
-  <p>
-    <?php echo $mySingleQuoteHelloString; ?>
-  </p>
-  <h3>Double Quoted</h3>
-  <p>
-    <?php echo $myDoubleQuoteHelloString; ?>
-  </p>
+    <h1>Random Trivia Question</h1>
+    <h2>Random Trivia Question</h2>
+    <dl>
+        <dt>Category</dt>
+        <dd>
+            <?php echo $randomTrivia->category; ?>
+        </dd>
+        <dt>Difficulty</dt>
+        <dd>
+            <?php echo $randomTrivia->difficulty; ?>
+        </dd>
+        <dt>Question</dt>
+        <dd>
+            <?php echo $randomTrivia->question; ?>
+        </dd>
+        <dt>Answer!</dt>
+        <dd>
+            <?php echo $randomTrivia->correct_answer; ?>
+       </dl>
 </body>
 </html>
